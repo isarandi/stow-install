ddf7a49bd457da6db0e6bda9e64f3727fcb3d216
István Sárándi <sarandi@vision.rwth-aachen.de>
2023-11-06 21:10:52 +0100

    Update setup_stow_install.sh

--- setup_stow_install.sh
+++ setup_stow_install.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.local
-cd ~/.local
+pushd ~/.local
 mkdir -p bin_priority bin doc etc games include info lib lib/pkgconfig lib32 lib64 libexec libx32 man sbin share share/aclocal share/man src stow var
+popd
 
 echo '' >> ~/.bashrc
 cat .bashrc_stow >> ~/.bashrc
